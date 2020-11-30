f9962e2a4ccae795348cc3e9fcfe0b9ad1ce1997
Octavio Quiroz <octavio.lopez@beexponential.com.mx>
2020-11-29 19:27:13 -0600

    muestra la ayuda al usuario para el uso de la herramienta

---
+++ src/commands/help.ts
@@ -0,0 +1,20 @@
+const help = {
+    echo: (): void => {}
+}
+help.echo = () => {
+    const message = `
+    mussa es una aplicación cli para manejar proyectos de netsuite (suiteapps y appcustomization), siendo una capa superior del sdfcli oficial de netsuite, permite realizar acciones
+y procesos que no están incluidos en el sdfcli o simplemente enlanzado comandos dentro de un mismo flujo.
+
+* Lista de comandos disponibles:
+    > mussa [-c] --create-project <nombre proyecto>     inicializa un nuevo proyecto SDF vacío
+    > mussa [-a] --access                               solicitud de acceso a Netsuite
+    > mussa [-a] --token                                autenticación por token de mussa a Netsuite
+    > mussa [-s] --script                               crear nuevo script [userevent, clientscript, map/reduce, scheduled, suitelet, restlet]
+    > mussa [-u] --upload <ruta del archivo>            cargar un archivo al FileCabinet de Netusite 
+    > mussa [-d] --deploy                               despliegue de un proyecto SDF a la base de Nesuite configurada
+`
+    console.log(message);
+}
+
+export default help;
